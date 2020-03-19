99cb4f556e7f0946cbd463aaca4c14eb17928291
„wojtek <wojciech_lisowski@wp.pl>
2020-03-19 21:10:46 +0100

    software development

--- project1/src/containers/Blog/Blog.js
+++ project1/src/containers/Blog/Blog.js
@@ -9,7 +9,8 @@ import './Blog.css';
 class Blog extends Component {
     state ={
         posts:[],
-        selectedPostId: null
+        selectedPostId: null,
+        error: false
       }
 
     componentDidMount(){
@@ -19,26 +20,32 @@ class Blog extends Component {
             const updatedPosts = posts.map(post =>{
                 return{
                     ...post,
-                    author:'Max'
+                  //  author:'Max'
                 }
             });
             this.setState({posts: updatedPosts});
                 
-        });
+        })
+        .catch(error=>{
+            this.setState({error:true});
+    });
+}
         
-    }
+    
     postSelectedHandler = (id)=>{
         this.setState({selectedPostId: id});
     }
  
     render () {
-        const posts = this.state.posts.map(post =>{
+        let posts =<p>something went Wrong</p>;
+        if (!this.state.error){
+         posts = this.state.posts.map(post =>{
             return <Post 
             key={post.id} 
             title={post.title} 
             author={post.author}
             clicked={()=>this.postSelectedHandler(post.id)}/>
-        }) 
+        }); }
 
         return (
             <div>
